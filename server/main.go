0ad6bfea93d74bf369aeaef27ce9661dc602f4ae
dltndn <james98099@gmail.com>
2024-01-15 01:08:45 +0900

    feat(server): cors 허용

--- server/main.go
+++ server/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	r.Use(cors.New(config))
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
